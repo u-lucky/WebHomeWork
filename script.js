dad5671830e22d3af6c303579755c518c4e48107
u-lucky <dimdimdimeh@gmail.com>
2025-05-12 23:40:12 +0700

    Complete UserFunc

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ function dva(num) {
 }
 dva(8800555355); // +?
 dva(-1000000); // -?
+
+
+function sumThree(a, b, c) {
+  console.log(a + b + c);
+}
+sumThree(1, 2, 3); // 6?
+
+function Sqrt(num) {
+  return Math.sqrt(num);
+}
+let res1 = Sqrt(3);
+let res2 = Sqrt(4);
+console.log(res1 + res2); 
+
+function checkEven(arr) {
+  for (let elem of arr) {
+    if (elem % 2 !== 0) return false;
+  }
+  return true;
+}
+console.log(checkEven([2, 4, 6]));
+console.log(checkEven([2, 50000001, 6]));
+
+function roadToLimit(num) {
+  let count = 0;
+  while (num >= 10) {
+    num /= 2;
+    count++;
+  }
+  return count;
+}
+console.log(roadToLimit(50)); 
